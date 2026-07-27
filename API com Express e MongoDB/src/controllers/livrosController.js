5e1b6e598c14ce1e2b697c91de942d23ccdcb2d2
Ericao327 <ericlopesdemedeiros@gmail.com>
2026-07-27 20:41:02 -0300

    Reutilizando paginação

--- API com Express e MongoDB/src/controllers/livrosController.js
+++ API com Express e MongoDB/src/controllers/livrosController.js
@@ -7,26 +7,11 @@ class LivroController {
   static listarLivros = async (req, res, next) => {
     try {
 
-      let { limite = 5, pagina = 1, odenacao = "_id:-1" } = req.query;
+      const buscaLivros = livros.find();
 
-      let [campoOdenacao, ordem] = odenacao.split(":");
+      req.resultado = buscaLivros;
 
-      limite = parseInt(limite);
-      pagina = parseInt(pagina);
-      ordem = parseInt(ordem);
-
-      if (limite > 0 && pagina > 0) {
-        const livrosResultado = await livros.find()
-          .sort({ [campoOrdenacao]: ordem })
-          .skip((pagina - 1) * limite)
-          .limit(limite)
-          .populate("autor")
-          .exec();
-
-          res.status(200).json(livrosResultado);
-      } else {
-        next(new RequisicaoIncorreta());
-      }
+      next(); 
 
     } catch (erro) {
       next(erro);
@@ -100,11 +85,13 @@ class LivroController {
       const busca = await processaBusca(req.query);
 
       if (busca !== null) {
-        const livrosResultado = await livros
+        const livrosResultado = livros
       .find(busca)
       .populate("autor");
 
-      res.status(200).send(livrosResultado);
+      req.resultado = livrosResultado;
+
+      next();
       } else {
         res.status(200).send([]);
       }
