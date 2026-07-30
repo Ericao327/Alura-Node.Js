024e7638a177b07a0a084d9ec798b70b76e33691
Ericao327 <ericlopesdemedeiros@gmail.com>
2026-07-30 15:31:56 -0300

    Configurando opções de pesquisa

--- alura-books/src/componentes/Pesquisa/index.js
+++ alura-books/src/componentes/Pesquisa/index.js
@@ -3,38 +3,44 @@ import styled from 'styled-components'
 import { useState } from 'react'
 
 const PesquisaContainer = styled.section`
-        background-image: linear-gradient(90deg, #002F52 35%, #326589 165%);
-        color: #FFF;
-        text-align: center;
-        padding: 85px 0;
-        height: 270px;
-        width: 100%;
+    background-image: linear-gradient(90deg, #002F52 35%, #326589 165%);
+    color: #FFF;
+    text-align: center;
+    padding: 85px 0;
+    height: 270px;
+    width: 100%;
 `
 
 const Titulo = styled.h2`
-        color: #FFF;
-        font-size: 36px;
-        text-align: center;
-        width: 100%;
+    color: #FFF;
+    font-size: 36px;
+    text-align: center;
+    width: 100%;
 `
 
 const Subtitulo = styled.h3`
-        font-size: 16px;
-        font-weight: 500;
-        margin-bottom: 40px;
+    font-size: 16px;
+    font-weight: 500;
+    margin-bottom: 40px;
 `
 
 function Pesquisa() {
-    const [ textoDigitado, setTextoDigitado ] = useState('')
+    const [textoDigitado, setTextoDigitado] = useState('')
 
     return (
         <PesquisaContainer>
             <Titulo>Já sabe por onde começar?</Titulo>
-            <Subtitulo>Encontre seu livro em nossa estante.</Subtitulo>
+
+            <Subtitulo>
+                Encontre seu livro em nossa estante.
+            </Subtitulo>
+
             <Input
                 placeholder="Escreva sua próxima leitura"
-                onBlur={evento => setTextoDigitado(evento.target.value)}
+                value={textoDigitado}
+                onChange={(evento) => setTextoDigitado(evento.target.value)}
             />
+
             <p>{textoDigitado}</p>
         </PesquisaContainer>
     )
