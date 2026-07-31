ee768cfccbdd2c3933535f28c8fceed462df1781
Ericao327 <ericlopesdemedeiros@gmail.com>
2026-07-30 22:31:10 -0300

    Atualizando os estados com o onBlur e inserindo componentes dinâmicos

--- alura-books/src/componentes/Pesquisa/index.js
+++ alura-books/src/componentes/Pesquisa/index.js
@@ -1,13 +1,14 @@
 import Input from '../Input'
 import styled from 'styled-components'
 import { useState } from 'react'
+import { livros } from './dadosPesquisa'
 
 const PesquisaContainer = styled.section`
     background-image: linear-gradient(90deg, #002F52 35%, #326589 165%);
     color: #FFF;
     text-align: center;
     padding: 85px 0;
-    height: 270px;
+    min-height: 270px;
     width: 100%;
 `
 
@@ -15,7 +16,6 @@ const Titulo = styled.h2`
     color: #FFF;
     font-size: 36px;
     text-align: center;
-    width: 100%;
 `
 
 const Subtitulo = styled.h3`
@@ -24,8 +24,32 @@ const Subtitulo = styled.h3`
     margin-bottom: 40px;
 `
 
+const Resultado = styled.div`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    margin-bottom: 20px;
+    cursor: pointer;
+
+    p {
+        width: 200px;
+    }
+
+    img {
+        width: 100px;
+    }
+
+    &:hover {
+        border: 1px solid white;
+    }
+`
+
 function Pesquisa() {
-    const [textoDigitado, setTextoDigitado] = useState('')
+    const [textoPesquisa, setTextoPesquisa] = useState("")
+
+    const livrosFiltrados = livros.filter((livro) =>
+        livro.nome.toLowerCase().includes(textoPesquisa.toLowerCase())
+    )
 
     return (
         <PesquisaContainer>
@@ -37,11 +61,18 @@ function Pesquisa() {
 
             <Input
                 placeholder="Escreva sua próxima leitura"
-                value={textoDigitado}
-                onChange={(evento) => setTextoDigitado(evento.target.value)}
+                value={textoPesquisa}
+                onChange={(evento) =>
+                    setTextoPesquisa(evento.target.value)
+                }
             />
 
-            <p>{textoDigitado}</p>
+            {livrosFiltrados.map((livro) => (
+                <Resultado key={livro.nome}>
+                    <img src={livro.src} alt={livro.nome} />
+                    <p>{livro.nome}</p>
+                </Resultado>
+            ))}
         </PesquisaContainer>
     )
 }
